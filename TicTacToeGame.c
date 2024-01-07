1c4de09b4c3902a5673fd66c6c9bfd0271cea0e8
Sachith Nimendra <126912947+sachithnim@users.noreply.github.com>
2024-01-07 11:07:14 +0530

    Add files via upload

--- TicTacToeGame.c
+++ TicTacToeGame.c
@@ -1,7 +1,65 @@
 #include<Stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<time.h>
 
-int main(void){
+char board[3][3];	//globle vriables
+const char PLAYER = 'X';
+const char COMPUTER = 'O';
+
+void resetBord();
+void printBoard();
+int checkFreeSpace();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner(char);
+
+int main(){
 	
+	char win = ' ';		//empty, no winner
+	
+	resetBord();
+	printBoard();
 	
 	return 0;
+}
+
+void resetBord(){
+	
+	int i, j;
+	
+	for(i = 0; i < 3; i++){		//nested for loop
+		
+		for(j = 0; j < 3; j++){
+			board[i][j] = ' ';		//empty cahr
+		}
+	}
+}
+
+void printBoard(){
+	
+	printf(" %c  |  %c | %c ", board[0][0], board[0][1], board[0][2]);
+	printf("\n----|----|----\n");
+	
+	printf(" %c  | %c  | %c ", board[1][0], board[1][1], board[1][2]);
+	printf("\n----|----|----\n");
+	
+	printf(" %c  | %c  | %c ", board[2][0], board[2][1], board[2][2]);
+	printf("\n");
+}
+
+int checkFreeSpace(){
+}
+
+void playerMove(){
+}
+
+void computerMove(){
+}
+
+char checkWinner(){
+}
+
+void printWinner(char win){
 }
